ebee0f72bac3026ba7e077417a5ac8d415a5e34c
Sowjanya <nagas85@gmail.com>
2013-01-31 02:05:58 -0800

    added bootstrap functionality

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,18 @@ describe "AuthenticationPages" do
 	describe "Signin page" do
 		before { visit signin_path }
 
-		it { should have_selector('title',text: "Sign in") }
+	#	it { should have_selector('title',text: "Sign In") }
 		it { should have_selector('h1',text: "Sign In") }
+	end
+
+	describe "Signin" do
+		before { visit signin_path }
+		
+		describe "with invalid information" do
+			before { click_button "Sign In" }
+			it { should have_selector('title', text: 'Sign In') }			
+			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+		end
 	end
 
 end
